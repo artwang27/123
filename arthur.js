3742e7d5a4746e5e83d35d8d4fd1b1d9a657931b
artwang27 <71919659+artwang27@users.noreply.github.com>
2021-05-23 14:55:24 +0800

    15

---
+++ arthur.js
@@ -0,0 +1,40 @@
+let spiderOn=true;
+
+const myBody = document.getElementById('myBody');
+
+window.onload = init;
+
+
+function init(){
+    setSpider();
+
+    setTimeout(setAfterTwoSecond,2000); //兩秒後設定
+    setTimeout( clearSpider, 5000);
+}
+
+
+//兩秒後設定，此時才提供取消蜘蛛網的清除
+function setAfterTwoSecond(){
+    myBody.addEventListener('mousemove', clearSpider);
+}
+
+
+
+
+
+function setSpider(){
+    spiderOn = true;
+    myBody.style.backgroundImage = "url('spiderNet.jpg')";
+    myBody.style.backgroundRepeat ="no-repeat";
+    myBody.style.backgroundSize="cover";
+    //alert("ok");
+}
+
+function clearSpider(){
+    if( !spiderOn) return;
+
+    myBody.style.backgroundImage = "none";
+    myBody.style.backgroundColor = "whitesmoke";
+
+    spiderOn=false;
+}
