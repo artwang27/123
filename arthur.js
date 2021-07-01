56a26e501ee3925add74845fcc78886032d8ca27
artwang27 <71919659+artwang27@users.noreply.github.com>
2021-07-01 08:34:34 +0800

    30

--- arthur.js
+++ arthur.js
@@ -12,15 +12,20 @@ function print(data){
     console.log(data);
 }
 
+let sec=0;
 
 function init() {
     showDay();
-    let leftDay=showCountDown();    //倒數幾天
+    let leftDay=showCountDownByDay();    //倒數幾天
+    
     
     //takeBreak2(leftDay);
 
     //drawIt2();
     //window.location.href = 'getVar.html?id=123&user=Arthur';
+
+    //showCountDownBySecond();
+    setInterval( showCountDownBySecond, 90);
 
 }
 
@@ -38,12 +43,15 @@ function onmouseDown(){
     stopAnimation();
 }
 
-//離結業式倒數幾天
-function showCountDown(){
-    let countDown = document.getElementById("countDown");
+
+//離結業式倒數幾天，用常春藤來表示
+function showCountDownByDay(){
+    let countDown = document.getElementById("countDownDay");
 
     let leftDay = DateDiff("2021/7/2", new Date())+1;
     console.log(leftDay+"days");
+
+
     //countDown.innerHTML = "離結業式倒數 " + leftDay;
     let leaf = "<img src='countDown/leaf02.png' style='width:64px ; height:64px' >";
 
@@ -56,6 +64,36 @@ function showCountDown(){
     return leftDay;
 
 }    
+
+//離結業式倒數幾秒，用碼表來表示
+function showCountDownBySecond() {
+    let countDown = document.getElementById("countDownSecond");
+
+    /*
+    var t1 = new Date(YYYY, MM, DD, 0, 0, 0, 0);
+    var t2 = new Date(ZZZZ, NN, EE, 0, 0, 0, 0);
+    var dif = t1.getTime() - t2.getTime();
+
+    var Seconds_from_T1_to_T2 = dif / 1000;
+    var Seconds_Between_Dates = Math.abs(Seconds_from_T1_to_T2);
+    */
+
+    let endTime=new Date(2021,6,1,9,50)
+    let cur=new Date();
+    let diff= (endTime.getTime() - cur.getTime())/1000;
+
+
+
+    console.log(diff);
+
+    //sec+=0.3;
+
+
+
+    countDown.innerHTML = diff; //秀出剩幾秒
+
+}
+
 
 
 //秀出防疫第幾天
